0d2b8cb345cc7f04faebdbe2e0fce7a32f05e1f6
ciyuxu <ciyuxu@gmail.com>
2018-02-09 11:11:09 +0100

    implemented onfocus

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -3,6 +3,25 @@ import "./Searchbar.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 class Searchbar extends PureComponent {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      input: "",
+      border: "1px solid grey"
+    };
+  }
+
+  updateSearch(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  onFocus(event) {
+    this.setState({
+      border: "2px solid grey"
+    });
+  }
   render() {
     return (
       <div className="Searchbar">
@@ -12,6 +31,10 @@ class Searchbar extends PureComponent {
             placeholder="Search..."
             type="text"
             alt="Type your search here"
+            value={this.state.input}
+            onChange={this.updateSearch.bind(this)}
+            style={{ border: this.state.border }}
+            onFocus={this.onFocus.bind(this)}
           />
           <button className="Search-button" type="submit">
             <i className="fa fa-search fa-2x" aria-hidden="true" />
